2c3fbced0189ae281590064c4a60fc06d041c800
Alihassan1110 <107312581+Alihassan1110@users.noreply.github.com>
2022-09-24 20:15:48 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Hello World")
+
+st.write("This is a test")
+
+st.markdown('''
+                * This is a test
+                * This is a test
+                * This is a test    ''')
